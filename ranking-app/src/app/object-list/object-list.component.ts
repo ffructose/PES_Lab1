ad4a062790960513ce85697c7d20a94a64f1bcb8
Anna Hnatiuk <losers2004@gmail.com>
2024-09-26 20:14:52 +0300

    components

---
+++ ranking-app/src/app/object-list/object-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-object-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './object-list.component.html',
+  styleUrl: './object-list.component.css'
+})
+export class ObjectListComponent {
+
+}
